2dacde89fb8f68306018f5f32849abda920347ca
lazaropedro <lazaro.p806@gmail.com>
2024-12-12 14:05:03 -0300

    refino

--- assets/scripts/pages/home-page.js
+++ assets/scripts/pages/home-page.js
@@ -7,14 +7,14 @@ const intervalo = setInterval(function() {
     minutos = String(minutos).padStart(2, '0');
     segundos = String(segundos).padStart(2, '0');
 
-    document.getElementById("contador").innerHTML = "0 Dias 00:" + minutos + ":" + segundos;
-    if(tempoRestante==0){
-        document.getElementById("promocao").innerHTML = "Voçê perdeu a promoção!!"
-    }
+    
+
     if(tempoRestante>0){
+        document.getElementById("contador").innerHTML = "0 Dias 00:" + minutos + ":" + segundos;
         tempoRestante--; 
     }
-    else{
+    else if(tempoRestante==0){
+        document.getElementById("promocao").innerHTML = "Voçê perdeu a promoção!!"
         let antigo = document.getElementsByClassName('preco-antigo');
         let novo = document.getElementsByClassName('preco-novo');
 
